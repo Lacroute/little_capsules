9b4925a6036ab9fac4cb1b83cb31c7939af174f8
Lacroute <procureur.clement@gmail.com>
2016-09-20 18:04:51 +0200

    first attempt

---
+++ Assets/Scripts/WaterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour {
+
+
+	public GameObject P_WaterSurface;
+	public int WorldPadding;
+
+	private List<GameObject> _WaterSurfaces;
+
+
+	void Start () {
+		Vector3 refer = GameObject.Find("C_Terrain").GetComponent<Renderer>().bounds.center;
+		int width = (int) Mathf.Abs (refer.x) * 2 + WorldPadding;
+		int height = (int) Mathf.Abs (refer.z) * 2 + WorldPadding;
+
+		_WaterSurfaces = new List<GameObject> ();
+
+		for (int i = - width / 2 ; i < WorldPadding; i++) {
+			for(int k = - height / 2; k < WorldPadding; k++){
+				_WaterSurfaces.Add (Instantiate (
+					P_WaterSurface,
+					new Vector3(i, 0, k),
+					Quaternion.identity,
+					transform
+				));
+			}
+
+		}
+	}
+	
+
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/WaterSurfaceBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSurfaceBehavior : MonoBehaviour {
+
+	private float _seed;
+	private Vector3 _pos;
+
+
+	void Start () {
+		_seed = Random.Range (0.5f, 0.8f);
+		_pos = transform.position;
+	}
+	
+
+	void Update () {
+		transform.position = new Vector3(_pos.x, Mathf.PerlinNoise(_seed * Time.time, _seed * Time.time) ,_pos.z);
+	}
+}
